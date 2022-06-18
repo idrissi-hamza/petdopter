7a135352f57c39391c1f475f0e30d434ba46ae84
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-06-18 22:38:59 +0200

    add default font family

--- components/Layout.js
+++ components/Layout.js
@@ -5,17 +5,21 @@ const Layout = ({ title, keywords, description, children }) => {
     <div>
       <Head>
         <title>{title}</title>
-        <meta name="description" content={description}/>
+        <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       {children}
     </div>
   );
 };
-Layout.defaultProps={
-  title:"Pets Market | Find your pet",
-  description:"Find your pet",
-  keywords:'cat,dog, pet,adopt'
-}
+Layout.defaultProps = {
+  title: "Pets Market | Find your pet",
+  description: "Find your pet",
+  keywords: "cat,dog, pet,adopt",
+};
 
 export default Layout;
