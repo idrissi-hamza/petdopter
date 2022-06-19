391e4f5b77d5c0458d2cdab4c030f7b535dbf739
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-06-19 22:16:15 +0200

    fetch data for a single pet

--- pages/offers/[slug].js
+++ pages/offers/[slug].js
@@ -1,16 +1,29 @@
 import { useRouter } from "next/router";
 import Layout from "../../components/Layout";
+import { API_URL } from "../../config";
 
-const OffersPage = () => {
-  const { query } = useRouter();
-  const { slug } = query;
-  
+const OffersPage = ({ pet }) => {
+  const {
+    query: { slug },
+  } = useRouter();
+  // const { slug } = query;
+
   return (
     <Layout>
-      <h1>My Offers</h1>
-      <p>{slug}</p>
+      <p>{pet.name}</p>
     </Layout>
   );
 };
 
 export default OffersPage;
+
+export async function getServerSideProps({ query: { slug } }) {
+  const res = await fetch(`${API_URL}/api/offers/${slug}`);
+  const pets = await res.json();
+
+  return {
+    props: {
+      pet: pets[0],
+    },
+  };
+}
