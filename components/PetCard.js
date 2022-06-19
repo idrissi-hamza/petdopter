c48ec246b1a667952f1235c7ec1f7aedfdc8bb5b
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-06-19 21:47:20 +0200

    navigate from petcard to a offers/slug pages

--- components/PetCard.js
+++ components/PetCard.js
@@ -3,17 +3,21 @@ import Image from "next/image";
 
 const PetCard = ({ pet }) => {
   return (
-    <div className=" group cursor-pointer transition duration-200  transform sm:hover:scale-105 border border-purple-500 bg-purple-500 rounded-xl overflow-hidden">
-      <Image
-        src={pet.image ? pet.image : "/images/pet-default.jpg"}
-        width={920}
-        height={800}
-      />
-      <div className="p-2 h-14 flex items-center justify-center text-xl ">
-        <p className="truncate max-w-md text-purple-50 font-semibold text-center ">{pet.name}</p>
-      </div>
-    </div>
+    <Link href={`/offers/${pet.slug}`}>
+      <a className=" group cursor-pointer transition duration-200  transform sm:hover:scale-105 border border-purple-500 bg-purple-500 rounded-xl overflow-hidden">
+        <Image
+          src={pet.image ? pet.image : "/images/pet-default.jpg"}
+          width={920}
+          height={800}
+        />
+        <div className="p-2 h-14 flex items-center justify-center text-xl ">
+          <p className="truncate max-w-md text-purple-50 font-semibold text-center ">
+            {pet.name}
+          </p>
+        </div>
+      </a>
+    </Link>
   );
-}; 
+};
 
 export default PetCard;
