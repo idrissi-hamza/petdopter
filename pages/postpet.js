152ea6327930d5bccdfb4a35e7bfd00d2ed3aad1
A-IdrissiHamza <a.idrissihamza@gmail.com>
2022-07-24 21:43:56 +0100

    post pet ui style

--- pages/postpet.js
+++ pages/postpet.js
@@ -17,12 +17,12 @@ export default function AddPetPage() {
     { value: "protective", label: "Protective" },
   ];
   const breedsOptions = [
-    { value: "Labrador Retriever", label: "Affectionate" },
-    { value: "French Bulldogs", label: "Playful" },
-    { value: "Retrievers (Golden)", label: "Curious" },
-    { value: "German Shepherd Dogs", label: "Independent" },
-    { value: "Poodles", label: "Friendly" },
-    { value: "Bulldogs", label: "Protective" },
+    { value: "labradorRetriever", label: "Labrador Retriever" },
+    { value: "frenchBulldogs", label: "French Bulldogs" },
+    { value: "RetrieversGolden", label: "Retrievers (Golden)" },
+    { value: "germanShepherdDogs", label: "German Shepherd Dogs" },
+    { value: "Poodles", label: "Poodles" },
+    { value: "Bulldogs", label: "Bulldogs" },
   ];
 
   const [values, setValues] = useState({
@@ -58,96 +58,96 @@ export default function AddPetPage() {
 
   return (
     <Layout title="Add New Pet">
-      <Link href="/pets">Go Back</Link>
-      <h1>Add Pet</h1>
-      <form onSubmit={handleSubmit} className="">
-        <div className=" flex flex-col gap-4 p-8">
-          <div>
-            <label htmlFor="name">Pet Name</label>
-            <input
-              type="text"
-              id="name"
-              name="name"
-              value={values.name}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="flex">
-            <label htmlFor="performers">Breed </label>
-            <Select options={breedsOptions} />
-          </div>
-          <div>
-            <label htmlFor="performers">Adoption fees</label>
-            <input
-              type="text"
-              name="performers"
-              id="performers"
-              value={values.performers}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="venue">Gender</label>
-            <input
-              type="text"
-              name="venue"
-              id="venue"
-              value={values.venue}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="venue">Color</label>
-            <input
-              type="text"
-              name="venue"
-              id="venue"
-              value={values.venue}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div className="flex">
-            <label htmlFor="performers">Characteristics </label>
-            <Select
-              options={characteristicsOptions}
-              // onChange={(option) => setAssignedUsers(option)}
-              isMulti
-            />
-          </div>
-          <div>
-            <label htmlFor="address">Address</label>
-            <input
-              type="text"
-              name="address"
-              id="address"
-              value={values.address}
-              onChange={handleInputChange}
-            />
-          </div>
-          <div>
-            <label htmlFor="date">Birth date</label>
-            <input
-              type="date"
-              name="date"
-              id="date"
-              value={values.date}
-              onChange={handleInputChange}
-            />
-          </div>
+      <div className="p-6 mx-auto mt-10 border-2">
+        <Link href="/pets">
+          <a className="text-blue-500 font-semibold"> Go Back</a>
+        </Link>
+        <h1 className="text-4xl text-purple-700 text-center m-6">Add Pet</h1>
+        <form onSubmit={handleSubmit} className="">
+          <div className=" flex flex-col gap-4 p-8">
+            <div className="flex gap-4">
+              <label htmlFor="name">Pet Name</label>
+              <input
+                type="text"
+                id="name"
+                name="name"
+                value={values.name}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="flex gap-4 items-center">
+              <label htmlFor="performers">Breed </label>
+              <Select className="w-60" options={breedsOptions} />
+            </div>
+            <div className="flex gap-4">
+              <label htmlFor="address">Address</label>
+              <input
+                type="text"
+                name="address"
+                id="address"
+                value={values.address}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="flex gap-4">
+              <label htmlFor="characteristics">Characteristics </label>
+              <Select
+                className="w-60"
+                options={characteristicsOptions}
+                onChange={(option) => handleInputChange(option)}
+                isMulti
+              />
+            </div>
+            <div className="flex gap-4">
+              <label htmlFor="performers">Adoption fees</label>
+              <input
+                type="text"
+                name="performers"
+                id="performers"
+                value={values.fees}
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="flex gap-4">
+              <label htmlFor="venue">Gender</label>
+              <input
+                type="text"
+                name="venue"
+                id="venue"
+                value={values.gender}
+                onChange={handleInputChange}
+              />
+            </div>
 
-          <div>
-            <label htmlFor="description">Pet Description</label>
-            <textarea
-              type="text"
-              name="description"
-              id="description"
-              value={values.description}
-              onChange={handleInputChange}
-            ></textarea>
+            <div className="flex gap-4">
+              <label htmlFor="date">Birth date</label>
+              <input
+                type="date"
+                name="date"
+                id="date"
+                value={values.date}
+                onChange={handleInputChange}
+              />
+            </div>
+
+            <div className="flex gap-4">
+              <label htmlFor="description">Pet Description</label>
+              <textarea
+                type="text"
+                name="description"
+                id="description"
+                value={values.description}
+                onChange={handleInputChange}
+              ></textarea>
+            </div>
+            <input
+              type="submit"
+              value="Add Pet"
+              className="bg-purple-500 p-2 rounded mx-36 text-purple-100 mt-10"
+            />
           </div>
-          <input type="submit" value="Add Pet" className="btn" />
-        </div>
-      </form>
+        </form>
+      </div>
     </Layout>
   );
 }
